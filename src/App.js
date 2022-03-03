a63d8b9ce856abbf02d8631f6b892f9c98eda323
kaveh moradian <kave.moradian@gmail.com>
2022-03-03 18:48:20 +0330

    header and pages created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from "react"
+import { Layout } from 'antd';
+
+//import route
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//import syles
 import "./Assets/style/App.less";
+
+//import pages
+import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+
+//import components
+import Header from "./Components/Header/Header"
+
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="first">
-        <h1>hello</h1>
-      </header>
-    </div>
+    <>
+    <Layout>
+    <Router>
+      <Header/>
+        <Content style={{marginTop:50}}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
+    </>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Layout } from "antd";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../../Assets/images/logo-and-type-white.png";
+const { Header } = Layout;
+
+function Head() {
+    return (
+        <Header
+        style={{ position: "fixed", zIndex: 2, width: "100%" }}
+        className="header">
+            <div
+                style={{ float: "right", width: "160px", marginLeft: "14px", margin: "-5px 0px 0 12px"}}
+            >
+                <Link to="/">
+                <img src={Logo} alt="logo" style={{ width: "100%" }} />
+                </Link>
+            </div>
+            <div className="secounder-mobile-menu">
+                <NavLink
+                activeClassName="menu-item-selected"
+                className="menu-item-prnx"
+                exact
+                to="/">
+                صفحه اصلی
+            </NavLink>
+          </div>
+        </Header>
+    )
+}
+
+export default Head;
