2ee8f6f7b68fda980c08e559fa39c3b350fae798
kaveh moradian <kave.moradian@gmail.com>
2022-03-03 23:48:58 +0330

    create first structure of home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Col, Row } from "antd";
+import { Link } from "react-router-dom";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Row justify="center">
+        <Col>
+          <Link to="/register">
+            <h1>پیش ثبت‌نام بازی gta v</h1>
+          </Link>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Home;
